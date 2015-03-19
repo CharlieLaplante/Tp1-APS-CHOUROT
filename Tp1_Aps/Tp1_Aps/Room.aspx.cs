4c36195ee9fbbf6938ee2723433d29e43897add8
PouletGrill <xavierb.2006@hotmail.com>
2015-03-19 09:43:59 -0400

    5$

---
+++ Tp1_Aps/Tp1_Aps/Room.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tp1_Aps
+{
+   public partial class Room : System.Web.UI.Page
+   {
+      protected void Page_Load(object sender, EventArgs e)
+      {
+         PersonnesTable personnesTable = new PersonnesTable((string)Application["MainDB"], this);
+         personnesTable.SelectAll();
+         personnesTable.MakeGridView(PN_GridView, "ModifierPersonne2.aspx");
+         // Pas besoin d'appeler personnesTable.EndQuerySql car c'est déjà fait dans personnesTable.MakeGridView(...)
+      }
+   }
+}
