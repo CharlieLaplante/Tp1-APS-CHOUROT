7e4a6be7d43391567008647bc30bd6eefc6f83d6
CharlieLaplante <201356187@P103-19.clg.priv>
2015-04-13 13:46:10 -0400

    eqwrq
    we

--- Tp1_Aps/Tp1_Aps/ChatRoom.aspx.cs
+++ Tp1_Aps/Tp1_Aps/ChatRoom.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlExpressUtilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,8 +13,47 @@ namespace Tp1_Aps
       protected void Page_Load(object sender, EventArgs e)
       {
          Thread ListeChat = new Thread((string)Application["MainDB"], this);
-         ListeChat.SelectTitle();
-         ListeChat.MakeGridView(PN_ListeChat, "");
-      }    
+         ListeChat.SelectAll();
+         
+         Table GridChatRoom =null;
+         MakeListeChat(GridChatRoom, ListeChat);
+      }   
+      private void MakeListeChat(Table Grid,Thread t)
+      {                  
+            while (t.Next())
+            {
+               TableRow tr = new TableRow();            
+               TableCell td = new TableCell();
+                     if (t.CellsContentDelegate[2] != null)
+                     {                      
+                        td.Controls.Add(t.CellsContentDelegate[2]());
+                     }
+                     else
+                     {
+                        Type type = t.FieldsTypes[2];
+                        if (SQLHelper.IsNumericType(type))
+                        {
+                           td.Text = t.FieldsValues[2].ToString();
+                           // IMPORTANT! Il faut inclure dans la section style
+                           // une classe numeric qui impose l'alignement à droite
+                           td.CssClass = "numeric";
+                        }
+                        else
+                        {
+                           td.Text = SQLHelper.FromSql(t.FieldsValues[2]);
+                        }                        
+                     }
+                     tr.Cells.Add(td);
+                 Grid.Rows.Add(tr);                  
+            }
+         
+         PN_ListeChat.Controls.Clear();
+
+         if (Grid != null)
+            PN_ListeChat.Controls.Add(Grid);
+         t.EndQuerySQL();
+         }
    }
+ 
+
 }
